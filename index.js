d04f0b791e0720c119f5a0c069dfdcc2ac2d6726
Daniil <daniil.vodenejev@khk.ee>
2022-04-20 14:11:38 +0300

    main.hbs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const hbs = require('express-handlebars');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
-    extename: 'hbs',
+    extname: 'hbs',
     defaultLayout: 'main',
     layoutsDir: __dirname + '/views/layouts/',
 }))
@@ -17,10 +17,13 @@ app.engine('hbs', hbs.engine({
 app.use(express.static('public'));
 
 const mysql = require('mysql')
-
 const bodyParser = require('body-parser')
+
 app.use(bodyParser.urlencoded({extended: true}))
 
+/*app.get('/', function(req,res){
+  res.send('Hello world')
+})*/
 //create db connection
 var con = mysql.createConnection({
     host: "localhost",
@@ -34,8 +37,8 @@ con.connect(function (err){
     console.log("Connected to joga_mysql db");
 })
 
-app.listen(3000, () => {
-    console.log("app is started at http://localhost:3000")
+app.listen(3001, () => {
+    console.log("app is started at http://localhost:3001")
 });
 
 
